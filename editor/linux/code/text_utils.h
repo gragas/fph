f99a19c7ae325d54d3a3c76856366669e6e7c6c7
Thomas D. Fischer <tdf.tomfischer@gmail.com>
2015-05-24 18:42:32 -0500

    Added cornflower and black font support. Added small, medium, and large font size support. Partially implemented these in the text engine (still need to implement these in text.cpp and text.h).

--- editor/linux/code/text_utils.h
+++ editor/linux/code/text_utils.h
@@ -1,13 +1,16 @@
 #include "SDL/SDL.h"
 #include "text.h"
 #include <array>
+#include <string>
 
 #ifndef TEXT_UTILS_H
 #define TEXT_UTILS_H
 
 namespace text_utils
 {
-  extern std::array<SDL_Surface*, 65> chars; // numeric, lower, upper
+  extern std::array<SDL_Surface*, 390> chars; // numeric, lower, upper
+  extern int counter;
+  void load( std::string path );
   void init();
 }
 
